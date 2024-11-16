0591f5b69e4447274d01fa5eb645691605a80d93
Jianwen WEI <weijianwen@gmail.com>
2024-11-16 12:41:17 +0800

    Chinese search does not work on sphinx-immaterial theme.

--- source/conf.py
+++ source/conf.py
@@ -32,6 +32,7 @@ release = '1.0.0'
 # ones.
 extensions = [
     'recommonmark',
+    'sphinx_immaterial',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,9 +56,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_immaterial'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+    # Do not download google fonts
+    # See "Extension error" https://github.com/jbms/sphinx-immaterial/issues/216
+    "font": False
+}
